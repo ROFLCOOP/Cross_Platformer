9b384d727e8d90fbd343c130b654c92b8911436e
Unknown <andrewckenny97@gmail.com>
2018-10-29 11:42:51 +1100

    Add Player & Controls
    controls for player are placeholder, for testing

---
+++ Cross-Platformer/Assets/Player_Control.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Control : MonoBehaviour {
+	[Tooltip("This will affect how fast the player moves")]
+	[Range(1, 100)]
+	public float Speed = 25.0f;
+
+	// Use this for initialization
+	void Start() {
+
+	}
+
+	Vector3 Velocity = new Vector3(0, 0, 0);
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.W))
+		{
+			Velocity += transform.forward * Speed * Time.deltaTime;
+		}
+		
+		if (Input.GetKey(KeyCode.S))
+		{
+			Velocity += -transform.forward * Speed * Time.deltaTime;
+		}
+
+		if (Input.GetKey(KeyCode.A))
+		{
+			Velocity += -transform.right * Speed * Time.deltaTime;
+		}
+
+		if (Input.GetKey(KeyCode.D))
+		{
+			Velocity += transform.right * Speed * Time.deltaTime;
+		}
+
+		if (Input.GetKey(KeyCode.Space))
+		{
+			transform.Translate(transform.up * Speed);
+		}
+
+		if(Velocity.x != 0 || Velocity.y != 0 || Velocity.z != 0)
+		{
+			Velocity.z += -Velocity.z * Time.deltaTime;
+			Velocity.x += -Velocity.x * Time.deltaTime;
+		}
+		transform.Translate(Velocity);
+	}
+}
