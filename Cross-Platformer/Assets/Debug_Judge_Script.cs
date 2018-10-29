443e644ad7df8cd644ea6d051873d6c85643f0ea
Unknown <sammydeedge@hotmail.com>
2018-10-29 11:27:29 +1100

    Created basic Input with debugging

---
+++ Cross-Platformer/Assets/Debug_Judge_Script.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debug_Judge_Script : MonoBehaviour {
+
+	void OnFire1()
+    {
+        Debug.Log("Fire1 button Pressed");
+    }
+}
---
+++ Cross-Platformer/Assets/PC_Controller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PC_Controller : MonoBehaviour {
+
+    public GameObject PC_Player1;
+
+    // Update is called once per frame
+    void Update () {
+
+		if (Input.GetButtonDown("Fire1"))
+        {
+            PC_Player1.SendMessage("OnFire1");
+        }
+
+	}
+}
