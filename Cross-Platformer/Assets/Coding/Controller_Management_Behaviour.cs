cb013c13f85edb56f461d48773210c0225cd821b
Unknown <sammydeedge@hotmail.com>
2018-11-01 10:29:09 +1100

    Player Select Screen Complete, info transfer to main screen needs work

--- Cross-Platformer/Assets/Coding/Controller_Management_Behaviour.cs
+++ Cross-Platformer/Assets/Coding/Controller_Management_Behaviour.cs
@@ -5,18 +5,45 @@ using UnityEngine.SceneManagement;
 
 public class Controller_Management_Behaviour : MonoBehaviour {
 
+    public GameObject PlayerObject_1;
+    public GameObject PlayerObject_2;
+
+    Color[] palette = new Color[6] { Color.red, Color.magenta, Color.blue, Color.cyan, Color.green, Color.yellow };
+
 
     bool m_findingPlayerControllers = true;
 
-    Dictionary<int, int> Joystick_Player_Map;
+    bool P1Ready = false;
+    bool P2Ready = false;
+
+    public struct PlayerInfo
+    {
+        public PlayerInfo(Controller_Management_Behaviour manager, int i)
+        {
+            PlayerNo = i;
+            Colour = Color.red;
+            playerObject = null;
+            if (PlayerNo == 0)         { playerObject = manager.PlayerObject_1; }
+            else if (PlayerNo == 1)    { playerObject = manager.PlayerObject_2; }
+            else                { Debug.Log("Error Player has no assigned object"); }
+        }
+        public int PlayerNo;
+        public Color Colour;
+        public GameObject playerObject;
+    }
+
+    public Dictionary<int, PlayerInfo> Joystick_Player_Map;
+
+    
 
     int currentNumberPlayers = 0;
 
     public string NextScene;
 
+
 	// Use this for initialization
 	void Start () {
-        Joystick_Player_Map = new Dictionary<int, int>();
+        Joystick_Player_Map = new Dictionary<int, PlayerInfo>();
 	}
 	
 	// Update is called once per frame
@@ -25,7 +52,8 @@ public class Controller_Management_Behaviour : MonoBehaviour {
         {
             RunFindControllerPlayer();
         }
-	}
+        
+    }
 
     void RunFindControllerPlayer()
     {
@@ -34,8 +62,14 @@ public class Controller_Management_Behaviour : MonoBehaviour {
             if (Input.GetButtonDown("Jump_" + joystickNo.ToString()))
             {
                 Debug.Log("Jump Button Pressed on" + joystickNo.ToString());
-                Joystick_Player_Map[currentNumberPlayers] = joystickNo;
-                currentNumberPlayers++;
+                if (!Joystick_Player_Map.ContainsKey(joystickNo))
+                {
+                    Joystick_Player_Map[joystickNo] = new PlayerInfo(this, currentNumberPlayers);
+                    Joystick_Player_Map[joystickNo].playerObject.SendMessage("ActivatePlayer");
+                    currentNumberPlayers++;
+                }
+
+                
             }
         }
 
@@ -46,6 +80,32 @@ public class Controller_Management_Behaviour : MonoBehaviour {
                 SceneManager.LoadScene(NextScene);
                 Debug.Log("Options Button Pressed on Player 1's controller");
             }
+        }
+    }
+
+    void AssignPlayerColour(int codeNo)
+    {
+        int joystick = codeNo / 10;
+        int colourCode = codeNo % 10;
+
+        Color hue = palette[colourCode];
+
+        PlayerInfo Data = Joystick_Player_Map[joystick];
+        Data.Colour = hue;
+        Joystick_Player_Map[joystick] = Data;
+    }
+
+    void ReadyPlayer (int player)
+    {
+        if (player == 1)        { P1Ready = true; }
+        else if (player == 2)   { P2Ready = true; }
+    }
+
+    void LoadScene()
+    {
+        if (P1Ready && P2Ready)
+        {
+            SceneManager.LoadScene(NextScene);
         }
     }
 }
