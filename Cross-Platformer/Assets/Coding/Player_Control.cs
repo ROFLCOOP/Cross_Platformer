24581a3447f088894f1cfb1b161f1d3a16bd3544
Unknown <andrewckenny97@gmail.com>
2018-11-01 16:25:31 +1100

    rework player movement, various level tweaks

--- Cross-Platformer/Assets/Coding/Player_Control.cs
+++ Cross-Platformer/Assets/Coding/Player_Control.cs
@@ -7,7 +7,8 @@ public class Player_Control : MonoBehaviour {
 	[Range(1, 100)]
 	public float Speed = 25.0f;
 
-	private bool spaceHeld = false;
+	//private bool spaceHeld = false;
+	private bool jumping = false;
 	private float timer = 0;
 	private const float timerIncrement = 0.001f;
 
@@ -16,11 +17,14 @@ public class Player_Control : MonoBehaviour {
 
 	}
 
+	
+
 	Vector3 Velocity = new Vector3(0, 0, 0);
 	Vector3 Jump = new Vector3(0, 0, 0);
 
 	// Update is called once per frame
 	void Update () {
+		Ray height = new Ray(transform.position, -transform.up);
 		if (Input.GetKey(KeyCode.W))
 		{
 			Velocity += transform.forward * Speed * Time.deltaTime;
@@ -41,38 +45,55 @@ public class Player_Control : MonoBehaviour {
 			Velocity += transform.right * Speed * Time.deltaTime;
 		}
 
-		if (Input.GetKey(KeyCode.Space) && !spaceHeld)
+		if (Input.GetKey(KeyCode.Space) && !jumping)
 		{
 			//transform.Translate(transform.up * Speed);
-			spaceHeld = true;
-			Jump.y = 1;
+			jumping = true;
+			//spaceHeld = true;
+			Jump.y = 0.5f;
 		}
 
-		if(spaceHeld || timer > 0.0f)
+		if (jumping)
 		{
-			if (!Input.GetKey(KeyCode.Space))
+			//if (!Input.GetKey(KeyCode.Space))
+			//{
+			//	spaceHeld = false;
+			//}
+			timer += Time.deltaTime;
+			if (timer < 0.5f)
 			{
-				spaceHeld = false;
-			}
-			timer += Time.deltaTime * timerIncrement;
-			if(timer < 1.0f && spaceHeld)
-			{
-				Jump.y -= 0.5f * Time.deltaTime;
+				Jump.y -= Time.deltaTime * 1.4f;
 			}
 			else
 			{
-				timer = 0.0f;
-				Velocity.y = 0;
+				Jump.y -= Time.deltaTime * 0.6f;
 			}
+			
 			transform.Translate(Jump);
+
+			
+			if (transform.position.y < 0)
+			{
+				jumping = false;
+				timer = 0.0f;
+			}
 		}
 
-		if(Velocity.x != 0 || Velocity.y != 0 || Velocity.z != 0)
+		if(Velocity.magnitude > 0)
 		{
 			Velocity.z += -Velocity.z * Time.deltaTime;
 			Velocity.x += -Velocity.x * Time.deltaTime;
-			Velocity.y += -Velocity.y * 8 * Time.deltaTime;
+			//Velocity.y += -Velocity.y * 8 * Time.deltaTime;
+
+			
 		}
 		transform.Translate(Velocity);
+
+		if (transform.position.y < 0)
+		{
+			Vector3 temp = transform.position;
+			temp.y = 0;
+			transform.position = temp;
+		}
 	}
 }
