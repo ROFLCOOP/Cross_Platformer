6505a6e52871269a03669b19757a7629a29b65b9
Unknown <andrewckenny97@gmail.com>
2018-11-02 16:18:54 +1100

    rewrote player controls, added better ground collision & jumping
    player control file is now formatted more neatly.
    player can now jump properly and has personal gravity.

--- Cross-Platformer/Assets/Coding/Player_Control.cs
+++ Cross-Platformer/Assets/Coding/Player_Control.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,7 +12,7 @@ public class Player_Control : MonoBehaviour {
 
 	//private bool spaceHeld = false;
 	private bool jumping = false;
-	private float timer = 0;
+	private float jumpDecelerationTimer = 0;
 	private const float timerIncrement = 0.001f;
 	private const int maxDistanceray = 1000;
 	private float height = 0;
@@ -28,115 +29,173 @@ public class Player_Control : MonoBehaviour {
 
 	GameObject ground;
 
-	void getHeight ()
+	void updateHeightFromGroundDetails ()
 	{
 		RaycastHit hit;
 		int layerMask = 1 << 8;
 		layerMask = ~layerMask;
-		if (Physics.Raycast(transform.position, -transform.up, out hit, maxDistanceray, layerMask))
+		if (Physics.Raycast(transform.position + new Vector3(0, 0.35f, 0), -transform.up, out hit, maxDistanceray, layerMask))
 		{
-			height = hit.distance;
+			height = hit.distance - 0.35f;
 			ground = hit.collider.gameObject;
 		}
 
 	}
 
-	void getInput()
+	Vector3 getInputAcceleration()
 	{
+		Vector3 inputVelocity = new Vector3();
 		if (Input.GetKey(KeyCode.W))
 		{
-			Velocity += transform.forward * Speed * Time.deltaTime;
+			inputVelocity += transform.forward * Speed * Time.deltaTime;
 		}
 
 		if (Input.GetKey(KeyCode.S))
 		{
-			Velocity += -transform.forward * Speed * Time.deltaTime;
+			inputVelocity += -transform.forward * Speed * Time.deltaTime;
 		}
 
 		if (Input.GetKey(KeyCode.A))
 		{
-			Velocity += -transform.right * Speed * Time.deltaTime;
+			inputVelocity += -transform.right * Speed * Time.deltaTime;
 		}
 
 		if (Input.GetKey(KeyCode.D))
 		{
-			Velocity += transform.right * Speed * Time.deltaTime;
+			inputVelocity += transform.right * Speed * Time.deltaTime;
 		}
 
 		if (Input.GetKey(KeyCode.Space) && !jumping)
 		{
 			jumping = true;
-			Jump.y = 15.0f;
+			inputVelocity.y = 1.0f;
+			jumpDecelerationTimer = 0;
 		}
+
+
+
+		return inputVelocity;
+
 	}
 
-	void getVelocity()
+	//void getVelocity()
+	//{
+	//	if (jumping)
+	//	{
+	//		timer += Time.deltaTime;
+	//		if (timer < 0.5f)
+	//		{
+	//			Jump.y -= Time.deltaTime * 15.0f;
+	//		}
+	//		else
+	//		{
+	//			Jump.y -= Time.deltaTime * 10.0f;
+	//		}
+	//
+	//		frameJumpVelocity = Jump * Time.deltaTime;
+	//
+	//		if (height < -frameJumpVelocity.y)
+	//		{
+	//			jumping = false;
+	//			Vector3 pos = transform.position;
+	//			pos.y -= height;
+	//			transform.position = pos;
+	//		}
+	//		else
+	//		{
+	//			transform.Translate(frameJumpVelocity);
+	//		}
+	//	}
+	//
+
+	private void applyGravity()
 	{
-		if (jumping)
+		if(jumping)
 		{
-			timer += Time.deltaTime;
-			if (timer < 0.5f)
+			jumpDecelerationTimer += Time.deltaTime;
+			if (jumpDecelerationTimer < 0.5f)
 			{
-				Jump.y -= Time.deltaTime * 15.0f;
+				Velocity.y -= Time.deltaTime * 2.5f;
 			}
 			else
 			{
-				Jump.y -= Time.deltaTime * 10.0f;
+				Velocity.y -= Time.deltaTime * 1.0f;
 			}
-
-			frameJumpVelocity = Jump * Time.deltaTime;
-
-			if (height < -frameJumpVelocity.y)
-			{
-				jumping = false;
-				Vector3 pos = transform.position;
-				pos.y -= height;
-				transform.position = pos;
-			}
-			else
-			{
-				transform.Translate(frameJumpVelocity);
-			}
-		}
-
-		if (Velocity.magnitude > 0)
-		{
-			Velocity.z += -Velocity.z * Time.deltaTime;
-			Velocity.x += -Velocity.x * Time.deltaTime;
-			if (!jumping && height > 0)
-			{
-				Velocity.y += -0.5f * Time.deltaTime;
-			}
-
-		}
-	}
-
-	void applyVelocity()
-	{
-		if (Velocity.y > height)
-		{
-			Velocity.y = height;
-			transform.Translate(Velocity);
-			Velocity.y = 0;
 		}
 		else
 		{
-			transform.Translate(Velocity);
+			Velocity.y -= Time.deltaTime * 1.0f;
+		}
+	}
+
+	void applyVelocityDampening()
+	{
+		
+		if (Velocity.magnitude > 0)
+		{
+			Velocity.z += -Velocity.z * Time.deltaTime * 2.0f;
+			Velocity.x += -Velocity.x * Time.deltaTime * 2.0f;
+			//if (!jumping && height > 0)
+			//{
+			//	Velocity.y += -0.5f * Time.deltaTime;
+			//}
+	
+		}
+	}
+	//
+	//void applyVelocity()
+	//{
+	//	if (Velocity.y > height)
+	//	{
+	//		Velocity.y = height;
+	//		transform.Translate(Velocity);
+	//		Velocity.y = 0;
+	//	}
+	//	else
+	//	{
+	//		transform.Translate(Velocity);
+	//	}
+	//}w
+
+	private void checkForGrounded()
+	{
+		if(-Velocity.y > height)
+		{
+			jumping = false;
+			Velocity.y = -height + 0.01f;
 		}
 	}
 
 	private void FixedUpdate()
 	{
-		getHeight();
-		Debug.Log(height);
+		updateHeightFromGroundDetails();
 
-		getInput(); // contains anything that checks for player input
+		Velocity += getInputAcceleration();
+		applyVelocityDampening();
 
-		getVelocity(); // changes velocity values where necessary
 
-		applyVelocity(); // applies all velocity
+
+
+		applyGravity();
+		checkForGrounded();
+
+		transform.Translate(Velocity);
+
+
+
+		//Debug.Log(height);
+		//
+		//getInput(); // contains anything that checks for player input
+		//
+		//getVelocity(); // changes velocity values where necessary
+		//
+		//applyVelocity(); // applies all velocity
 		
 	}
+
+	
+
+
 
 	// Update is called once per frame
 	void Update () {
