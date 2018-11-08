aa5ba1b117e9b265ba841dc1ae113282d8ef9983
Unknown <andrewckenny97@gmail.com>
2018-11-08 15:24:31 +1100

    Adjusted Cointroller to work with Main Scene

--- Cross-Platformer/Assets/Coding/Cointroller.cs
+++ Cross-Platformer/Assets/Coding/Cointroller.cs
@@ -6,8 +6,12 @@ using UnityEditor;
 
 public class Cointroller : MonoBehaviour {
 
-    Vector3[] hrr_coin_spots = new Vector3[7] { new Vector3(-9.87f, 15.33f, 2.35f), new Vector3(8.54f, 15.33f, 5.61f), new Vector3(-0.61f, 15.33f, 7.78f), new Vector3(22.1f, 7.76f, -1.73f), new Vector3(-3.55f, 7.76f, 10.62f), new Vector3(12.78f, 7.76f, 29.94f), new Vector3(-33.47f, 7.0f, 22.71f) };
-    //Vector3[] sky_coin_spots = new Vector3[7] { new Vector3(-9.87f, 10.33f, 2.35f), new Vector3(8.54f, 10.33f, 5.61f), new Vector3(-0.61f, 10.33f, 7.78f), new Vector3(22.1f, 2.76f, -1.73f), new Vector3(-3.55f, 2.76f, 10.62f), new Vector3(12.78f, 2.76f, 29.94f), new Vector3(-33.47f, 2.0f, 22.71f) };
+	const int HorrorCoinAmount	= 7;
+	const int SkyCoinAmount	= 9;
+
+    Vector3[] hrr_coin_spots = new Vector3[HorrorCoinAmount] { new Vector3(-9.87f, 15.33f, 2.35f), new Vector3(8.54f, 15.33f, 5.61f), new Vector3(-0.61f, 15.33f, 7.78f), new Vector3(22.1f, 7.76f, -1.73f), new Vector3(-3.55f, 7.76f, 10.62f), new Vector3(12.78f, 7.76f, 29.94f), new Vector3(-33.47f, 7.0f, 22.71f) };
+    // Vector3[] sky_coin_spots = new Vector3[SkyCoinAmount];
+
 
     string currSceneName;
 
@@ -43,15 +47,18 @@ public class Cointroller : MonoBehaviour {
 
     void moveCoin()
     {
-        int select = Random.Range(1, 8);
+        
         if (currSceneName == "HorrorLevel")
         {
-            transform.position = hrr_coin_spots[select];
+			int select = Random.Range(0, 7);
+			transform.position = hrr_coin_spots[select];
         }
         else if (currSceneName == "Main_Scene")
         {
-            //transform.Translate(sky_coin_spots[select]);
-            Debug.Log("Sky Level Coin Locations not set up yet!");
+			GameObject parent = GameObject.Find("CoinNodes");
+			int select = Random.Range(0, SkyCoinAmount + 1);
+			transform.position = parent.transform.GetChild(select).position;
+			Debug.Log("Sky Level Coin Locations not set up yet!");
         }
         else
         {
